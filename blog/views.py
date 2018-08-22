2d475e1cf5fae9e1c5d6bc83d273376f8cd581db
ramazan <koscheibismark@yandex.ru>
2018-08-22 21:33:38 +0300

    My lowFLow app, first commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
+def post_list(request):
+	post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+	return render(request, 'blog/post_list.html', {'posts': post_list})
+	
+	
